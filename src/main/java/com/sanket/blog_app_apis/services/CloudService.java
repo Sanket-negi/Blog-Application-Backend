c91f58e0da4e733ce6a38c83f2ccb2118cd41bcf
Sanket-negi <negisanket18@gmail.com>
2026-04-10 12:43:54 +0530

    updated application.properties

--- src/main/java/com/sanket/blog_app_apis/services/CloudService.java
+++ src/main/java/com/sanket/blog_app_apis/services/CloudService.java
@@ -1,11 +1,19 @@
 package com.sanket.blog_app_apis.services;
 
+import com.cloudinary.Cloudinary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Map;
+@Service
+public class CloudService {
+    @Autowired
+    private Cloudinary cloudinary;
 
-public interface CloudService {
-         String uploadFile(MultipartFile file);
+    public String uploadFile(MultipartFile file) throws IOException {
+        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), Map.of());
+        return uploadResult.get("url").toString();
+    }
 }
